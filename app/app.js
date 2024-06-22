51f695221a22d70e452d607037926c5f901041f7
Guy Arbitman <guy20495@gmail.com>
2024-06-22 21:00:45 +0300

    Support http2 and http2-tls servers

--- app/app.js
+++ app/app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const config = require('config')
 const https = require('https')
+const spdy = require('spdy')
 const fs = require('fs')
 const logger = require('app/logger')
 const partialResponse = require('express-partial-response')
@@ -45,6 +46,46 @@ const httpsOptions = {
 const httpsServer = https.createServer(httpsOptions, app)
 httpsServer.listen(config.tls_port, () => {
   const address = httpsServer.address()
+  logger.info(`listen ${address.address}:${address.port}`)
+});
+
+// HTTP2
+const http2Options = {
+  spdy: {
+    protocol: 'h2',
+    plain: true,
+    // **optional**
+    // Parse first incoming X_FORWARDED_FOR frame and put it to the
+    // headers of every request.
+    // NOTE: Use with care! This should not be used without some proxy that
+    // will *always* send X_FORWARDED_FOR
+    'x-forwarded-for': true
+  }
+}
+const http2Server = spdy.createServer(http2Options, app);
+http2Server.listen(config.http2_port, () => {
+  const address = http2Server.address()
+  logger.info(`listen ${address.address}:${address.port}`)
+});
+
+const http2TLSOptions = {
+  key: fs.readFileSync(process.env.HTTPS_KEY_FILE),
+  cert: fs.readFileSync(process.env.HTTPS_CERT_FILE),
+  spdy: {
+    protocols: ['h2'],
+    plain: false,
+    ssl: true,
+    // **optional**
+    // Parse first incoming X_FORWARDED_FOR frame and put it to the
+    // headers of every request.
+    // NOTE: Use with care! This should not be used without some proxy that
+    // will *always* send X_FORWARDED_FOR
+    'x-forwarded-for': true
+  }
+}
+const http2TLSServer = spdy.createServer(http2TLSOptions, app);
+http2TLSServer.listen(config.http2_tls_port, () => {
+  const address = http2TLSServer.address()
   logger.info(`listen ${address.address}:${address.port}`)
 });
 
--- config/default.js
+++ config/default.js
@@ -1,6 +1,8 @@
 module.exports = {
   port: 8080,
   tls_port: 8443,
+  http2_port: 8082,
+  http2_tls_port: 8442,
   logger: {
     level: 'info'
   },
